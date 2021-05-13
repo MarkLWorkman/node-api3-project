de4b73164ff33b4df6f43dd16fe66f93ce90f7cb
MarkLWorkman <70355546+MarkLWorkman@users.noreply.github.com>
2021-05-13 15:31:30 -0400

    added env information

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
+
 const server = require("./api/server");
 
-server.listen(4000, () => {
-  console.log("Server running on http://localhost:4000");
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
